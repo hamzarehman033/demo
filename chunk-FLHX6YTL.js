718a6b12b7d412d70c391301a75ccd6da31bb1f7
hamza.rehman <hamza.rehman@cloudsolutions.com.sa>
2026-07-03 18:26:10 +0300

    Auto-generated commit

---
+++ chunk-FLHX6YTL.js
@@ -0,0 +1,1 @@
+var t={production:!0,baseUrl:"https://localhost:7170/api",signalrHubUrl:"http://localhost:7170/hubs/device-data"};export{t as a};
--- chunk-OAPTGJFW.js
+++
@@ -1,1 +0,0 @@
-import{J as r,U as o,Z as s,c as n,ga as u}from"./chunk-ZV6LYP6I.js";function c(t){t||(s(c),t=o(u));let i=new n(e=>t.onDestroy(e.next.bind(e)));return e=>e.pipe(r(i))}export{c as a};
